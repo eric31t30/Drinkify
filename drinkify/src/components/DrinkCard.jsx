fb817f9e0c1117f88fecabe3bfc56e7d5bc7cf4e
Eric Mateo <eric31dev@gmail.com>
2025-11-26 17:22:03 -0500

    feat: add DrinkList and DrinkCard components ✨

---
+++ drinkify/src/components/DrinkCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DrinkCard() {
+  return (
+    <div>DrinkCard</div>
+  )
+}
+
+export default DrinkCard
---
+++ drinkify/src/components/DrinkList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DrinkList() {
+  return (
+    <div>
+
+        
+    </div>
+  )
+}
+
+export default DrinkList
