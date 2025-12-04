e44fce58b6989aa51ab5a1e2073145ecadced03b
Eric Mateo <eric31dev@gmail.com>
2025-12-04 14:22:52 -0500

    ✨ feat: Add /drink/:id endpoint to get individual drink data

--- drinkify/src/pages/DrinkDetails.jsx
+++ drinkify/src/pages/DrinkDetails.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import "../styles/drink-details.css";
 import { useParams } from "react-router-dom";
 
 function DrinkDetails() {
+
   const { id } = useParams();
+  const [drink, setDrink] = useState([])
+
+  useEffect(() => {
+
+    const getdrink = async()=>{
+        try{
+            const res = await fetch(`http://localhost:3000/drink/${id}`);
+            const data = await res.json()
+
+            setDrink(data)
+            console.log(data);
+            
+        }catch(err){
+            console.error("Error al cargar la bebida", err);   
+        }
+    }
+
+    getdrink();
+    
+  }, [id])
+  
 
   return (
     <div className="drink">
-      <h1>detalles de { id }</h1>
+      <h1>detalles de { drink.name }</h1>
     </div>
   );
 }
