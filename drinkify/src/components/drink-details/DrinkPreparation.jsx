eda50ccd4aa41df3f387832774bf381237d6fe30
Eric Mateo <eric31dev@gmail.com>
2025-12-11 13:58:58 -0500

    💄 style: improve DrinkPreparation responsive layout

--- drinkify/src/components/drink-details/DrinkPreparation.jsx
+++ drinkify/src/components/drink-details/DrinkPreparation.jsx
@@ -5,38 +5,36 @@ import clock from "/icons/clock.svg"
 
 function DrinkPreparation({ drink }) {
   return (
-    <div>
-      <section
-        className={styles["preparation"]}
-        style={{ background: drink.color }}
-      >
-        <article className={styles["preparation-cont"]}>
-          <h2>Preparacion</h2>
-
-          {drink.preparation.map((p, i) => (
-            <div className={styles["preparation-steps"]} key={i}>
-              <div className={styles["step-index"]}>
-                <p>{i + 1}</p>
-              </div>
-              <p className={styles["step"]}>{p}</p>
+    <section
+      className={styles["preparation"]}
+      style={{ background: drink.color }}
+    >
+      <article className={styles["preparation-cont"]}>
+        <h2 className={styles.title}>Preparacion</h2>
+        
+        {drink.preparation.map((p, i) => (
+          <div className={styles["preparation-steps"]} key={i}>
+            <div className={styles["step-index"]}>
+              <p>{i + 1}</p>
             </div>
-          ))}
-
-          <div className={styles["prep-time-cont"]}>
-            <img className={styles["prep-icon"]} src={clock} alt="" />
-            <p className={styles["prep-time"]}>
-              Tiempo estimado: <span>{drink.prepTime}</span>
-            </p>
+            <p className={styles["step"]}>{p}</p>
           </div>
-        </article>
+        ))}
 
-        <div className={styles["shadow-1"]}></div>
-        <div className={styles["shadow-2"]}></div>
+        <div className={styles["prep-time-cont"]}>
+          <img className={styles["prep-icon"]} src={clock} alt="" />
+          <p className={styles["prep-time"]}>
+            Tiempo estimado: <span>{drink.prepTime}</span>
+          </p>
+        </div>
 
-        <img className={styles.leafs} src={leafs} alt="" />
-        <div className={styles.circle}></div>
-      </section>
-    </div>
+      </article>
+
+      <div className={styles["shadow-1"]}></div>
+      <div className={styles["shadow-2"]}></div>
+      <img className={styles.leafs} src={leafs} alt="" />
+      <div className={styles.circle}></div>
+    </section>
   );
 }
 
