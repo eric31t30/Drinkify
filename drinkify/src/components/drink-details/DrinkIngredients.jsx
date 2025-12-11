95105d9b9e28181972f9528e44fa3ee3b7ab4905
Eric Mateo <eric31dev@gmail.com>
2025-12-11 13:17:08 -0500

    💄 style: improve DrinkIngredients responsive layout

--- drinkify/src/components/drink-details/DrinkIngredients.jsx
+++ drinkify/src/components/drink-details/DrinkIngredients.jsx
@@ -3,16 +3,16 @@ import styles from "../../styles/drink-ingredients.module.css";
 function DrinkIngredients({ drink }) {
   return (
     <section className={styles["ingredients-cont"]}>
-
-        <h2 className={styles["ingredients-title"]}>Ingred<span style={{color: drink.color}}>ientes</span></h2> 
-        <div className={styles.ingredients}>
-          {drink.ingredients.map((i) => (
-            <article className={styles.ingredient} key={i.name}>
-              <p className={styles["ingredient-name"]}>{i.name}</p>
-              <p className={styles["ingredient-amount"]}>{i.amount}</p>
-            </article>
-          ))}
-        </div>
+      
+      <h2 className={styles["ingredients-title"]}>Ingred<span style={{color: drink.color}}>ientes</span></h2> 
+      <div className={styles.ingredients}>
+        {drink.ingredients.map((i) => (
+          <article className={styles.ingredient} key={i.name}>
+            <p className={styles["ingredient-name"]}>{i.name}</p>
+            <p className={styles["ingredient-amount"]}>{i.amount}</p>
+          </article>
+        ))}
+      </div>
 
     </section>
   )
