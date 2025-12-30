7bba96e14e6f05b56ddc6dcaff8fec72e9d2486b
Eric Mateo <eric31dev@gmail.com>
2025-12-30 17:02:31 -0500

    💄 style: improve DrinkHero design

--- drinkify/src/components/drink-details/DrinkHero.jsx
+++ drinkify/src/components/drink-details/DrinkHero.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import styles from "../../styles/drink-hero.module.css";
 import { Link } from "react-router-dom";
 import { cloudinary } from "../../utils/Cloudinary";
@@ -12,6 +14,7 @@ import blob2 from "/illustrations/blob-2.svg";
 
 function DrinkHero({ drink }) {
 
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const scrollinfo = ()=>{
     document.getElementById("drink-info-scroll")?.scrollIntoView({
@@ -29,11 +32,33 @@ function DrinkHero({ drink }) {
         }}
       >
         <div className={styles["drink-hero"]}>
-          <img
-            className={styles["drink-image"]}
-            src={cloudinary(drink.images.png)}
-            alt={drink.name}
-          />
+          <figure className={styles["drink-image-cont"]}>
+            {!imageLoaded && (
+              <div className={styles["image-loader"]}>
+                <span className={styles["loader"]}></span>
+              </div>
+            )}
+
+            {drink.images.gallery.map((e, i) => (
+              <div key={e} className={`${styles["drink-image-wrapper"]} ${styles[`wrapper-${i + 1}`]}`}>
+                <img
+                  className={styles["drink-image"]}
+                  src={cloudinary(
+                    e,
+                    "c_fill,w_500,h_500,g_auto,f_auto,q_auto"
+                  )}
+                  alt={drink.name}
+                  onLoad={() => setImageLoaded(true)}
+                  onError={() => setImageLoaded(true)}
+                  style={{
+                    opacity: imageLoaded ? 1 : 0,
+                  }}
+                />
+              </div>
+            ))}
+
+          </figure>
+
           <article className={styles["drink-main-info"]}>
             <p className={styles["drink-category"]}>{drink.category}</p>
             <h1 className={styles["drink-name"]}>{drink.name}</h1>
@@ -70,8 +95,18 @@ function DrinkHero({ drink }) {
           Volver
         </Link>
 
-        <img className={`${styles["blobs"]} ${styles["blob-1"]}`} src={blob1} alt="" draggable="false" />
-        <img className={`${styles["blobs"]} ${styles["blob-2"]}`} src={blob2} alt="" draggable="false" />
+        <img
+          className={`${styles["blobs"]} ${styles["blob-1"]}`}
+          src={blob1}
+          alt=""
+          draggable="false"
+        />
+        <img
+          className={`${styles["blobs"]} ${styles["blob-2"]}`}
+          src={blob2}
+          alt=""
+          draggable="false"
+        />
         <img className={styles["dots"]} src={dots} alt="" draggable="false" />
         <img className={styles["leafs"]} src={leafs} alt="" draggable="false" />
         <div className={styles["circle"]}></div>
